aa84d6d813abc49385961ae621ac16e6c5e130a6
nithinK-142 <nithin.sagar.359@gmail.com>
2023-11-09 20:02:09 +0530

    update: hero component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
         {children}
       </body>
     </html>
