ea0aec5173ebbe787b6a36e7835911f7d24c5ba4
nithinK-142 <nithin.sagar.359@gmail.com>
2023-11-09 19:14:29 +0530

    add: assets.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
+import Skills from "@/components/main/Skills";
+
 export default function Home() {
-  return <main>Space Portfolio</main>;
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20 h-[1800px]">
+        <Hero />
+        <Skills />
+        <Encryption />
+        <Projects />
+      </div>
+    </main>
+  );
 }
---
+++ components/main/Encryption.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Encryption = () => {
+  return (
+    <div>Encryption</div>
+  )
+}
+
+export default Encryption
