c50fb8ced3d0888292eb504b64dd3a2011f4bad0
nithinK-142 <nithin.sagar.359@gmail.com>
2023-11-11 16:02:14 +0530

    update: dark reader blocker tag.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="darkreader-lock"></meta>
+      </head>
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
